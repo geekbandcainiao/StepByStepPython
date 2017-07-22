e53102031155ba70fead469824ac745b967e3cf1
weizhidaoshuo <weizhidaoshuo@weizhidaoshuodeMac-mini.local>
2017-07-22 17:59:59 +0800

    数据类型和变量

---
+++ StepByStepPython/P000001.py
@@ -0,0 +1,24 @@
+print('hello,world')
+
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+
+print(300)
+
+print(100 +200)
+
+print('100 +200 =', 100 +200)
+
+# 输入
+name = input()
+
+name2 = input('please enter your name:')
+print('hello',name2)
+
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+
+print('I\'m ok.')
+
